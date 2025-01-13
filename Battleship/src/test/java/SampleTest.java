7a7e05a4f67be6e80d921cf20a8882bac791c3d5
Schaubt <tristan.schaub@gmail.com>
2025-01-13 13:22:21 -0500

    Setup JUnit

---
+++ Battleship/src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    @DisplayName("Testing JUnit")
+    void main() {
+    }
+}
