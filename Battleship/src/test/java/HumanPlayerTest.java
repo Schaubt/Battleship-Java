9f092276a2f2a5b7b09941c5a64f27ac2cd61f84
Schaubt <tristan.schaub@gmail.com>
2025-02-06 14:45:42 -0500

    set up unit testing for human player

---
+++ Battleship/src/test/java/HumanPlayerTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class HumanPlayerTest {
+}
