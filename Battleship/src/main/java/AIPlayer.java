fe623a002b6f97cfacad01db3a6017fa38ed6f4d
Schaubt <tristan.schaub@gmail.com>
2025-02-13 14:22:45 -0500

    implement AIPlayer.attack()

--- Battleship/src/main/java/AIPlayer.java
+++ Battleship/src/main/java/AIPlayer.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class AIPlayer extends Player {
     String[] hitMissHistory;
@@ -17,7 +18,31 @@ public class AIPlayer extends Player {
         this.colOfShipDiscovery = -1;
         this.attackDirection = 'r';
     }
-    public void attack(){}
+    @Override
+    public boolean attack(int col, int row, Player targetPlayer){
+        boolean res = super.attack(col, row, targetPlayer);
+        if(Objects.equals(targetPlayer.bottomBoard.grid[row][col], "Hit")){
+            if(this.rowOfShipDiscovery == -1 && this.colOfShipDiscovery == -1 && this.attackDirection == 'r') {
+                this.rowOfShipDiscovery = row;
+                this.colOfShipDiscovery = col;
+                this.attackDirection = 'n';
+                this.shipProbeInProgress = true;
+            }
+            else if(this.shipProbeInProgress){
+                if(attackDirection == 'w' || attackDirection == 'e') this.attackOrientation = 'h';
+                else if(attackDirection == 'n' || attackDirection == 's') this.attackOrientation = 'v';
+
+            }
+        }
+        else if(Objects.equals(targetPlayer.bottomBoard.grid[row][col], "Miss")){
+            if(this.shipProbeInProgress){
+                if(this.attackDirection == 'n') this.attackDirection = 'w';
+                else if(this.attackDirection == 'w') this.attackDirection = 's';
+                else if(this.attackDirection == 's') this.attackDirection = 'e';
+            }
+        }
+        return res;
+    }
     public void determineAttackCoordinate(){
 
     }
--- Battleship/src/test/java/AIPlayerTest.java
+++ Battleship/src/test/java/AIPlayerTest.java
@@ -1,110 +1,126 @@
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AIPlayerTest {
     AIPlayer mockAIPlayer = new AIPlayer();
     Player mockOpponent = new HumanPlayer();
+    @Test
     public void AI_player_stores_row_of_a_ship_following_first_hit_and_no_current_target_ship(){
         int row = 5;
         int col = 5;
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(int col, int row, Player targetPlayer)
-        //int actual_row = mockAIPlayer.rowOfShipDiscovery;
-        //assertEquals(expected_row, actual_row);
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int actual_row = mockAIPlayer.rowOfShipDiscovery;
+        assertEquals(row, actual_row);
     }
+    @Test
     public void AI_player_stores_col_of_a_ship_following_first_hit_and_no_current_target_ship(){
         int col = 5;
         int row = 5;
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(int col, int row, Player targetPlayer)
-        //int actual_col = mockAIPlayer.colOfShipDiscovery;
-        //assertEquals(col, actual_col);
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int actual_col = mockAIPlayer.colOfShipDiscovery;
+        assertEquals(col, actual_col);
     }
+    @Test
     public void AI_player_attack_direction_is_north_upon_initial_enemy_ship_discovery(){
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'r';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'n';
-        //int actual = mockAIPlayer.attackDirection;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'r';
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'n';
+        int actual = mockAIPlayer.attackDirection;
+        assertEquals(expected, actual);
     }
+    @Test
     public void AI_player_attack_direction_switches_to_west_of_enemy_ship_discovery_following_north_coordinate_miss() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'n';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'w';
-        //int actual = mockAIPlayer.attackDirection;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'n';
+        mockOpponent.bottomBoard.grid[row][col] = null;
+        mockAIPlayer.shipProbeInProgress = true;
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'w';
+        int actual = mockAIPlayer.attackDirection;
+        assertEquals(expected, actual);
     }
+    @Test
     public void AI_player_attack_direction_switches_to_south_of_enemy_ship_discovery_following_west_coordinate_miss() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'w';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 's';
-        //int actual = mockAIPlayer.attackDirection;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'w';
+        mockOpponent.bottomBoard.grid[row][col] = null;
+        mockAIPlayer.shipProbeInProgress = true;
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 's';
+        int actual = mockAIPlayer.attackDirection;
+        assertEquals(expected, actual);
     }
+    @Test
     public void AI_player_attack_direction_switches_to_east_of_enemy_ship_discovery_following_south_coordinate_miss() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 's';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'e';
-        //int actual = mockAIPlayer.attackDirection;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 's';
+        mockOpponent.bottomBoard.grid[row][col] = null;
+        mockAIPlayer.shipProbeInProgress = true;
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'e';
+        int actual = mockAIPlayer.attackDirection;
+        assertEquals(expected, actual);
     }
+    @Test
     public void AI_player_determines_ship_is_vertical_if_attack_hits_directly_north_of_ship_discovery() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'n';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //mockOpponent.bottomBoard.grid[row+1][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'v';
-        //int actual = mockAIPlayer.attackOrientation;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'n';
+        mockAIPlayer.shipProbeInProgress = true;
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockOpponent.bottomBoard.grid[row+1][col] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'v';
+        int actual = mockAIPlayer.attackOrientation;
+        assertEquals(expected, actual);
     }
+    @Test
     public void AI_player_determines_ship_is_vertical_if_attack_hits_directly_south_of_ship_discovery() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 's';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //mockOpponent.bottomBoard.grid[row-1][col] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'v';
-        //int actual = mockAIPlayer.attackOrientation;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 's';
+        mockAIPlayer.shipProbeInProgress = true;
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockOpponent.bottomBoard.grid[row-1][col] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'v';
+        int actual = mockAIPlayer.attackOrientation;
+        assertEquals(expected, actual);
     }
     public void AI_player_determines_ship_is_horizontal_if_attack_hits_directly_west_of_ship_discovery() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'w';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //mockOpponent.bottomBoard.grid[row][col-1] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'h';
-        //int actual = mockAIPlayer.attackOrientation;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'w';
+        mockAIPlayer.shipProbeInProgress = true;
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockOpponent.bottomBoard.grid[row][col-1] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'h';
+        int actual = mockAIPlayer.attackOrientation;
+        assertEquals(expected, actual);
     }
     public void AI_player_determines_ship_is_horizontal_if_attack_hits_directly_east_of_ship_discovery() {
         int col = 5;
         int row = 5;
-        //mockAIPlayer.attackDirection = 'e';
-        //mockOpponent.bottomBoard.grid[row][col] = "Carrier";
-        //mockOpponent.bottomBoard.grid[row][col-1] = "Carrier";
-        //attack(col, row, targetPlayer)
-        //int expected = 'h';
-        //int actual = mockAIPlayer.attackOrientation;
-        //assertEquals(expected, actual);
+        mockAIPlayer.attackDirection = 'e';
+        mockAIPlayer.shipProbeInProgress = true;
+        mockOpponent.bottomBoard.grid[row][col] = "Carrier";
+        mockOpponent.bottomBoard.grid[row][col-1] = "Carrier";
+        mockAIPlayer.attack(col, row, mockOpponent);
+        int expected = 'h';
+        int actual = mockAIPlayer.attackOrientation;
+        assertEquals(expected, actual);
     }
     public void AI_player_attack_miss_on_coordinate_directly_north_of_vertical_ship_switches_attack_intention_to_south() {
     }
