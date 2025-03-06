2e84ff02e65737bcc42def6e8080b15838318588
Schaubt <tristan.schaub@gmail.com>
2025-03-06 15:43:06 -0500

    check if player is computer

--- Battleship/src/main/java/Player.java
+++ Battleship/src/main/java/Player.java
@@ -51,4 +51,7 @@ public abstract class Player {
         this.shipsAlive.put(ship.name, ship);
     }
     //public void removeShip(int row, int col, String orientation){}
+    public boolean isComputer() {
+        return this.isComputer;
+    }
 }
