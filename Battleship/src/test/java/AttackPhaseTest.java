0b6cc11592067ad7bf736b8922f23366c21bcd82
Schaubt <tristan.schaub@gmail.com>
2025-03-24 10:59:59 -0400

    test behavior of attackPhase.execute when player 2 is computer

--- Battleship/src/test/java/AttackPhaseTest.java
+++ Battleship/src/test/java/AttackPhaseTest.java
@@ -19,7 +19,7 @@ class AttackPhaseTest {
     String mockInput = mockRowNum + ", " + mockColNum;
     Player humanPlayer = new HumanPlayer();
     Player humanPlayer2 = new HumanPlayer();
-    Player AIPlayer = new AIPlayer();
+    AIPlayer AIPlayer = new AIPlayer();
     @Test
     public void executeAttackPhase_BothPlayersHaveShips_Player1IsHuman_InvokeGetUserInput() {
         InputStream sysInBackup = System.in; // backup System.in to restore it later
@@ -51,6 +51,31 @@ class AttackPhaseTest {
 
     @Test
     public void executeAttackPhase_BothPlayersHaveShips_Player2IsComputer_InvokeDetermineAttackCoordinate() {
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream(mockInput.getBytes());
+        System.setIn(in);
+        Input[] input = {
+                new Input(1, 1),
+                new Input(2, 1),
+                new Input(3, 1),
+                new Input(4, 1),
+                new Input(5, 1),
+        };
+
+        AttackPhase spyAttackPhase = Mockito.spy(attackPhase);
+
+        Player spyPlayer1 = Mockito.spy(humanPlayer);
+        spyPlayer1.bottomBoard = Mockito.spy(humanPlayer.bottomBoard);
+
+        AIPlayer spyPlayer2 = Mockito.spy(AIPlayer);
+        spyPlayer2.bottomBoard = Mockito.spy(AIPlayer.bottomBoard);
+
+        Mockito.when(spyAttackPhase.getUserInput(in)).thenReturn(input[0], input[1], input[2], input[3], input[4]);
+        Mockito.when(spyPlayer1.allShipsDestroyed()).thenReturn(false, true);
+        Mockito.when(spyPlayer2.allShipsDestroyed()).thenReturn(false, false);
+        spyAttackPhase.execute(spyPlayer1, spyPlayer2);
+        //verify(spyPlayer2).determineAttackCoordinate();
+        System.setIn(sysInBackup);
     }
 
     @Test
